9726aa84a489e802d7a78f1b3b7a136f6e4f978f
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 04:50:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,7 @@ end
 def apply_clearance(cart)
   cart.map {|label, item|
     if item[:clearance]
-      puts (item[:price] * 0.8 * 100).round
+      puts (item[:price] * 0.8 * 100).round / 100
       item[:price] = (item[:price] * 0.8 * 10000).round / 10000
     end
     [label, item]
