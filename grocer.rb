1cf090314f62fbabb8839653572777a877dcfa96
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 06:03:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -59,7 +59,7 @@ end
 
 def checkout(cart, coupons)
   cart = consolidate_cart(cart)
-  cart = apply_clearance(apply_coupons(cart, coupons))
+  cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
 
   cart_total = cart.reduce(0.0) { |sum, (label, item)| sum += item[:price] * item[:count] }
   cart_total > 100 ? cart_total *= 0.9 : cart_total
