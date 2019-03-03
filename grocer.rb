ab6b290fcdbacb4bbe97940001094c63d8c598d8
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 05:52:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,7 +60,6 @@ def checkout(cart, coupons)
   cart = consolidate_cart(cart)
   coupons = coupons.select {|coupon|
     found_item = find_cart_item(cart, coupon[:item])
-    puts found_item.inspect
     get_value(found_item)[:count] >= coupon[:num]
   }
   cart = apply_clearance(apply_coupons(cart, coupons))
