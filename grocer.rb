0f4347f990b8f4c316302264f34334a0966a4b4f
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 05:59:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,8 +24,6 @@ def apply_coupons(cart, coupons)
     cart_count = cart[name][:count]
     coupon_count = coupon[:num]
     if cart[name] && cart_count >= coupon_count
-
-
       amount_to_subtract = cart_count >= coupon_count ? coupon_count : cart_count
       amount_to_increment = cart["#{name} W/COUPON"] ? cart["#{name} W/COUPON"][:count] + 1 : 1
 
