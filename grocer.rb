421237c4273522eb451757cfdbf2ee41168845f5
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 05:49:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,9 +49,10 @@ def find_cart_item(cart, name)
   match = cart.find {|label, item|
     label.eql? name
   }
-  puts match.inspect
+  
   match[0] = [match[0]]
   match[1] = [match[1]]
+  puts match.inspect
   match.to_h
 end
 
