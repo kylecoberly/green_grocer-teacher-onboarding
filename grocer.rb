0cd19c464f418aa62a5abaecb5f8da1d36ccedca
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 04:18:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,10 +20,9 @@ def apply_coupons(cart, coupons)
     name = coupon[:item]
     cart_count = cart[name][:count]
     coupon_count = coupon[:num]
-    amount_to_subtract = cart_count >= coupon_count ? cart_count : coupon_count
+    amount_to_subtract = cart_count >= coupon_count ? coupon_count : cart_count
 
     cart[name][:count] = cart_count - amount_to_subtract
-    puts cart[name][:count], cart_count, coupon_count, amount_to_subtract
     cart["#{name} W/COUPON"] = {
       :price => coupon[:cost],
       :clearance => cart[name][:clearance],
