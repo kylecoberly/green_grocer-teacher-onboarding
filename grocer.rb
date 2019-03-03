0b41b664fed0cce93a94bbc9c18fe9f4b30885b1
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 03:52:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,29 @@
-def consolidate_cart(cart)
-  # code here
+require "pry"
+
+def get_label hash
+  hash.keys.first
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+def consolidate_cart(cart)
+  cart.reduce({}) {|totals, item|
+    name = get_label(item)
+    item[name][:count] ||= 0
+    item[name][:count] = item[name][:count] + 1
+    totals[name] = item[name]
+
+    totals
+  }
+end
+
+def apply_coupons(cart, coupon)
+  coupons.reduce(cart) {|cart, coupon|
+    name = coupon[:item]
+    binding.pry
+    cart[name][:count] -= coupon[:count]
+    cart["#{name} W/COUPON"] = cart[name]
+
+    cart
+  }
 end
 
 def apply_clearance(cart)
