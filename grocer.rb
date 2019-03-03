bd8254445067128d1ad69942663bc9665715f698
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 05:22:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  cart = consolidate_cart cart
+  cart = consolidate_cart(cart)
   coupons = coupons.filter {|coupon| cart[coupon[:item]][:count] >= coupon[:num] }
   cart = apply_clearance(apply_coupons(cart, coupons))
 
